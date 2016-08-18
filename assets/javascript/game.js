720c11198041031dbb57a3bcff92369777f3636d
Kevin Zhu <kzoo223@gmail.com>
2016-08-17 21:49:06 -0400

    initial on keypress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,30 @@
 //Variables & arrays
 	var words = ["terra", "locke", "sabin", "edgar", "shadow", "cyan"]
-
-	var guesses ="";
+	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 	var lives = 5;
-
-	
 
 //select random answer
 	var word = words[Math.floor(Math.random() * words.length)];
-	
-	console.log (word)
 
 //masking answer
-	var hidden = word.replace(/a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9/g,"_");
+	var hidden = [];	
 
+//functions
 
+document.onkeyup = function (event){
+	//letter input
+	var key = String.fromCharCode(event.keyCode).toLowerCase();
+	for (var i = 0; i<word.length; i++){
+		if(key == word[i]){
+			hidden[i] = key+"";
+		
+		}
+		else if (key != word[i]){
+			lives - 1
+		}
 
-
-
+	}
+}
 
 
 
